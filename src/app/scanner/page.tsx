80c62a0c906b88604c657b8ef3426a8e7e4e825d
Nata J. Negara <pranatajayanegara@gmail.com>
2025-03-24 15:27:34 +0700

    testing received qr value0.2

--- src/app/scanner/page.tsx
+++ src/app/scanner/page.tsx
@@ -1,15 +1,20 @@
 "use client";
 
-import { Scanner } from "@yudiel/react-qr-scanner";
-// import { useState } from "react";
+import { IDetectedBarcode, Scanner } from "@yudiel/react-qr-scanner";
+import { useState } from "react";
 
 const ScannerPage = () => {
-  //   const [scannedQR, setScannedQR] = useState<string | null>(null);
+  const [scannedQR, setScannedQR] = useState<string | null>(null);
 
   return (
     <div className="w-3xs h-64">
-      <Scanner onScan={(result) => console.log(result)} />
-      {/* {scannedQR && <p>{scannedQR}</p>} */}
+      <Scanner
+        scanDelay={5000}
+        onScan={(result: IDetectedBarcode[]) =>
+          setScannedQR(result[0].rawValue)
+        }
+      />
+      {scannedQR && <p>{scannedQR}</p>}
     </div>
   );
 };
